58f34c52b8494fc8c60c583bd39707cabf184e60
Cbupatel <patelshibu6311@gmail.com>
2026-07-02 12:26:08 +0530

    Update Firebase storage bucket URL

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCpUSDGCOQ37D5CSMSTnmRPdfEsrLKQM0M",
   authDomain: "connethu-1c8bb.firebaseapp.com",
   projectId: "connethu-1c8bb",
-  storageBucket: "connethu-1c8bb.firebasestorage.app",
+  storageBucket: "connethu-1c8bb.appspot.com",
   messagingSenderId: "891592275465",
   appId: "1:891592275465:web:6c75be8b62de6c6af9aad8",
   measurementId: "G-7JG5HV772M"
