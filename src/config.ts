2f9bed95655001405f0cc8c3181a3f1d0eb9a56a
Cbupatel <patelshibu6311@gmail.com>
2026-07-02 12:19:44 +0530

    Add Firebase configuration to config.ts
    Initialize Firebase configuration with required settings.

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpUSDGCOQ37D5CSMSTnmRPdfEsrLKQM0M",
+  authDomain: "connethu-1c8bb.firebaseapp.com",
+  projectId: "connethu-1c8bb",
+  storageBucket: "connethu-1c8bb.firebasestorage.app",
+  messagingSenderId: "891592275465",
+  appId: "1:891592275465:web:6c75be8b62de6c6af9aad8",
+  measurementId: "G-7JG5HV772M"
+};
